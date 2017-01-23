22cbd0b5d40606f9e9b5e56ca22b812cdcb98d38
Vincenzo Altieri <vin.altieri@gmail.com>
2017-01-22 19:11:39 -0500

    exercise solutions

---
+++ 101_109_exercises/easy4/leap_year.rb
@@ -0,0 +1,19 @@
+# leap_year.rb
+
+def leap_year?(year)
+
+end
+
+puts leap_year?(2016)
+puts leap_year?(2015)
+puts leap_year?(2100)
+puts leap_year?(2400)
+puts leap_year?(240000)
+puts leap_year?(240001)
+puts leap_year?(2000)
+puts leap_year?(1900)
+puts leap_year?(1752)
+puts leap_year?(1700)
+puts leap_year?(1)
+puts leap_year?(100)
+puts leap_year?(400)
---
+++ 101_109_exercises/easy4/multisum.rb
@@ -0,0 +1,13 @@
+# sum_multiples.rb
+
+def multisum(number)
+  array_to_sum = []
+  initial_array = Array(1..number)
+  array_to_sum = initial_array.select { |number| number % 3 == 0 || number % 5 == 0 }
+  array_to_sum.reduce(0, :+)
+end
+
+puts multisum(3)
+puts multisum(5)
+puts multisum(10)
+puts multisum(1000)

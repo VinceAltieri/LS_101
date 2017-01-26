469d624b4f9aeaeb356a3d28a63a3b6d93448ee8
Vincenzo Altieri <vin.altieri@gmail.com>
2017-01-25 21:35:18 -0500

    exercise solutions

--- 101_109_exercises/easy4/leap_year2.rb
+++ 101_109_exercises/easy4/leap_year2.rb
@@ -1,17 +1,36 @@
 # leap_year2.rb
 
-def leap_year?(year)
+# Understanding the problem:
 
+# Leap years before 1752 - every year divisble by 4 is a leap year.
+# 1752 and after, leap years as defined in the previous problem.
+# Seems like a simple addition of an if statement may work.
+# Test to see if the year entered into the function is before 1752 or >= 1752.
+
+
+def leap_year?(year)
+  (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
 end
 
-puts leap_year?(2016)
-puts leap_year?(2100)
-puts leap_year?(2400)
-puts leap_year?(240000)
-puts leap_year?(240001)
-puts leap_year?(2000)
-puts leap_year?(1752)
-puts leap_year?(1700)
-puts leap_year?(1)
-puts leap_year?(100)
-puts leap_year?(400)
+
+def leap_year2?(year)
+  if year < 1752
+    year % 4 == 0
+  else
+    (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+  end
+end
+
+puts leap_year2?(2016) == true
+puts leap_year2?(2015) == false
+puts leap_year2?(2100) == false
+puts leap_year2?(2400) == true
+puts leap_year2?(240000) == true
+puts leap_year2?(240001) == false
+puts leap_year2?(2000) == true
+puts leap_year2?(1900) == false
+puts leap_year2?(1752) == true
+puts leap_year2?(1700) == true
+puts leap_year2?(1) == false
+puts leap_year2?(100) == true
+puts leap_year2?(400) == true
--- 101_109_exercises/easy4/string_to_integer.rb
+++ 101_109_exercises/easy4/string_to_integer.rb
@@ -1,1 +1,29 @@
 # string_to_integer.rb
+
+# understanding the problem
+
+# input is a string of digits example '1234'
+# input should be converted to an integer
+# Integer or to_i methods can not be used
+# all characters are assumed to be numeric, no worry about invalid characters
+# most likely then no need to worry about edge cases, (emptiness etc.)
+# method should anlayze each character in the string.
+
+# pseudocode / approach ideas
+
+# create a conversion table hash for numbers 0-9 i.e. { '1' => 1 }, etc.
+# iterate through the input string.
+# get current character
+# convert to number from hash look-up table
+# continue for the length of the string
+
+def str_to_int(number_string)
+  number_lookup = { '1': 1, '2': 2, '3': 3, '4': 4, '5': 5,
+                    '6': 6, '7': 7, '8': 8, '9': 9
+                  }
+
+  number_string.split('')
+  number_string.each do {
+
+  }
+

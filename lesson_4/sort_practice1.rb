508ae3352f6997a230e59d418e71f23e4ef92554
Vincenzo Altieri <vin.altieri@gmail.com>
2017-01-29 14:38:54 -0500

    lesson_4 practice

---
+++ lesson_4/sort_practice1.rb
@@ -0,0 +1,6 @@
+# sort_practice1.rb
+
+['arc', 'bat', 'cape', 'ants', 'cap'].sort
+
+# answer
+# should return ['ants', 'arc', 'bat', 'cap', 'cape']
---
+++ lesson_4/sort_practice2.rb
@@ -0,0 +1,8 @@
+# sort_practice2.rb
+
+[['a', 'cat', 'b', 'c'], ['b', 2], ['a', 'car', 'd', 3], ['a', 'car', 'd']].sort
+
+# answer
+
+# should return:
+# [['a', 'car', 'd'], ['a', 'car', 'd', 3], ['a', 'car', 'd'], ['b', 2] ]

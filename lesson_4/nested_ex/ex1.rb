959a0bb25afafd11efc0d6b179a13284238d7e18
Vincenzo Altieri <vin.altieri@gmail.com>
2017-02-09 20:15:49 -0500

    lesson 4 exercises

---
+++ lesson_4/nested_ex/ex1.rb
@@ -0,0 +1,9 @@
+# ex1.rb
+
+arr1 = ['a', 'b', ['c', ['d', 'e', 'f', 'g']]]
+
+arr1[2][1][3]
+
+arr2 = [{first: ['a', 'b', 'c'], second: ['d', 'e', 'f']}, {third: ['g', 'h', 'i']}]
+
+arr2[1][:third] = 'g'

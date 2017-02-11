e8165946cd451106e5dd22e54b49f7ce1c108047
Vincenzo Altieri <vin.altieri@gmail.com>
2017-02-11 09:10:43 -0500

    changed folders due to lesson change

--- lesson_4/nested_ex/ex1.rb
+++
@@ -1,9 +0,0 @@
-# ex1.rb
-
-arr1 = ['a', 'b', ['c', ['d', 'e', 'f', 'g']]]
-
-arr1[2][1][3]
-
-arr2 = [{first: ['a', 'b', 'c'], second: ['d', 'e', 'f']}, {third: ['g', 'h', 'i']}]
-
-arr2[1][:third] = 'g'
